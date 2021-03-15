d7bc8e3bd47b382b49ecc295520a8c1ef2600038
S662 <or.sanchez100@gmail.com>
2021-03-14 20:35:50 -0400

    add style fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   add_flash_types :alert2
 
+
+  def after_sign_up_path_for(resource)
+    redirect_to new_profile_path
+  end
+
   private
 
   def record_not_found
